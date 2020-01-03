09196445afe54c3047c4e98d244b509fc82324d9
Dmitry Khusnutdinov <dron@cds-management.com>
2020-01-03 18:22:10 +0300

    add actual db link

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 module.exports = {
 	//'url' : 'mongodb://<dbuser>:<dbpassword>@novus.modulusmongo.net:27017/<dbName>'
-	'url' : 'mongodb://root:a18706e94f@localhost:27017/test'
+	'url' : 'mongodb://test:test@localhost:27017/test'
 }
